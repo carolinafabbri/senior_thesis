172338ac793fb9f1a2b9234e1cd3fdd632295ca8
Carolina <carolina@rouxbe.com>
2022-02-16 12:52:05 -0500

    Save progress 1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
+  devise_for :users
+  root 'site#home'
+
   get 'about', to: 'site#about'
   get 'home', to: 'site#home'
 
-  root to: 'site#home'
+  #root to: 'site#home'
 end
